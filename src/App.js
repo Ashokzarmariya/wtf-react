6603e2c4c2ab9b8fabc441abf7c2572c53e9f541
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-09-23 14:42:46 +0530

    set head

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import GymDetail from "./Components/Gyms/GymDetail";
 import Gyms from "./Components/Gyms/Gyms";
 
 
 function App() {
+  useEffect(() => {
+    document.title = 'WTF | Fitness Simplified | Making india Fit on a Budget';
+  });
   return (
     <div className="">
       <Routes>
